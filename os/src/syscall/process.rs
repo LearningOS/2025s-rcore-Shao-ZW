d79d9f28c1689a216b942a63839a2bae81281f08
shao <779224180@qq.com>
2025-03-31 15:32:01 +0800

    ch3

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,6 @@
 //! Process management syscalls
 use crate::{
-    task::{exit_current_and_run_next, suspend_current_and_run_next},
+    task::{exit_current_and_run_next, suspend_current_and_run_next, TASK_MANAGER},
     timer::get_time_us,
 };
 
@@ -41,5 +41,25 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => {
+            let ptr = _id as *const u8;
+            unsafe { *ptr as isize }
+        }
+        1 => {
+            let ptr = _id as *mut u8; 
+            unsafe { *ptr = _data as u8 }
+            0
+        }
+        2 => {
+            let inner = TASK_MANAGER.inner.exclusive_access();
+            let current = inner.current_task;
+            let retval = inner.tasks[current].syscall_times[_id];
+            drop(inner);
+            retval as isize
+        }
+        _=> {
+            -1
+        }
+    }
 }
